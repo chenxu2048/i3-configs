a511338f7f7bb3bc4d0ba591c295b6f75847ab15
Chx <chenxu2048@gmail.com>
2020-11-05 21:09:51 +0800

    use polybar

--- init.sh
+++ init.sh
@@ -3,14 +3,17 @@
 CONFIG_DIR=$HOME/.config
 
 I3_DIR=$CONFIG_DIR/i3
-I3_STATUS_DIR=$CONFIG_DIR/i3status
-ROFI_DIR=$CONFIG_DIR/rofi
-PICOM_DIR=$CONFIG_DIR/picom
+
+function mkconfig() {
+    local submodule="$1"
+    ln -s "$I3_DIR/configs/${submodule}" "$CONFIG_DIR/${submodule}"
+}
 
 I3_CONFIG=https://github.com/chenxu2048/i3-configs
 mkdir -p $I3_DIR
 git clone $I3_CONFIG $I3_DIR
 
-ln -s $I3_DIR/configs/i3status $I3_STATUS_DIR
-ln -s $I3_DIR/configs/rofi $ROFI_DIR
-ln -s $I3_DIR/configs/picom $PICOM_DIR
+# mkconfig i3status
+mkconfig picom
+mkconfig rofi
+mkconfig i3blocks
